3c63730f34a606a51cd28bd5ec78107b823bb915
Jeevan Yohan Varghese <jeevan@Jeevans-MacBook-Pro.local>
2023-01-20 00:22:19 +0530

    fix: ui fix and colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -5,7 +5,8 @@ import 'hexcolor_util.dart';
 class AppColors {
   static Color primaryColor = HexColor('0F0F10');
   static Color secondaryColor = HexColor('FFFFFF');
-  static Color highlightColor = HexColor('5D63FF');
+  // static Color highlightColor = HexColor('5D63FF');
+  static Color highlightColor = HexColor('466FFF');
   static Color cardBgColor = HexColor('1E1D1D');
   static Color cardBgColor2 = HexColor('1E1D1D');
 
