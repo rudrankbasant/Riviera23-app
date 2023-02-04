cebfa0bb42213c1c9cefb9641196f24b6def9809
rudrankbasant <rudrank123@gmail.com>
2023-02-04 23:37:29 +0530

    feat: enhanced merch section

--- lib/presentation/screens/merch_screen.dart
+++ lib/presentation/screens/merch_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:riviera23/cubit/merch/merch_cubit.dart';
 import 'package:riviera23/utils/app_colors.dart';
@@ -16,6 +17,13 @@ class MerchScreen extends StatefulWidget {
 }
 
 class _MerchScreenState extends State<MerchScreen> {
+  var showSizeChart = false;
+
+  @override
+  void initState() {
+    super.initState();
+    showSizeChart = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,8 +47,6 @@ class _MerchScreenState extends State<MerchScreen> {
               return Padding(
                   padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
                   child: GridView.builder(
-                      // crossAxisCount: 2,
-                      // mainAxisSpacing: 10,
                       shrinkWrap: true,
                       itemCount: merchList.length,
                       scrollDirection: Axis.vertical,
@@ -51,7 +57,14 @@ class _MerchScreenState extends State<MerchScreen> {
                             (MediaQuery.of(context).size.height * 0.7),
                       ),
                       itemBuilder: (context, index) {
-                        return getMerchCard(merchList[index], context);
+                        return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                showSizeChart = false;
+                              });
+                              showBottomMerchScreen(context, merchList[index], showSizeChart);
+                            },
+                            child: getMerchCard(merchList[index], context));
                       }));
             } else {
               return const Center(
@@ -66,17 +79,13 @@ class _MerchScreenState extends State<MerchScreen> {
 }
 
 Widget getMerchCard(Merch merch, BuildContext context) {
-  return GestureDetector(
-    onTap: () {
-      showPopUpMerch(merch, context);
-    },
-    child: Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        children: [
-          Container(
-            height: 160,
-            child: ClipRRect(
+  return Padding(
+    padding: const EdgeInsets.all(12.0),
+    child: Column(
+      children: [
+        Container(
+          height: 160,
+          child: ClipRRect(
               borderRadius: BorderRadius.circular(8.0),
               child: CachedNetworkImage(
                 height: 160,
@@ -99,138 +108,247 @@ Widget getMerchCard(Merch merch, BuildContext context) {
                   'assets/placeholder.png',
                   fit: BoxFit.fitWidth,
                 ),
-              )
-            ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            merch.name,
-            style: GoogleFonts.sora(
-                color: Colors.white, fontWeight: FontWeight.w500, fontSize: 17),
-          ),
-          Text(
-            "Rs ${merch.price}",
-            style: GoogleFonts.sora(
-                color: AppColors.highlightColor,
-                fontWeight: FontWeight.w500,
-                fontSize: 12),
-          )
-        ],
-      ),
+              )),
+        ),
+        SizedBox(height: 10),
+        Text(
+          merch.name,
+          style: GoogleFonts.sora(
+              color: Colors.white, fontWeight: FontWeight.w500, fontSize: 17),
+        ),
+        Text(
+          "Rs ${merch.price}",
+          style: GoogleFonts.sora(
+              color: AppColors.highlightColor,
+              fontWeight: FontWeight.w500,
+              fontSize: 12),
+        )
+      ],
     ),
   );
 }
 
-
-
-showPopUpMerch(Merch merch, BuildContext context) {
-  showDialog(
+void showBottomMerchScreen(BuildContext context, Merch merch, showSizeChart) {
+  showModalBottomSheet(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      isScrollControlled: true,
       context: context,
       builder: (context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(
-                20.0,
+        return StatefulBuilder(builder: (context, setState) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
               ),
+              color: AppColors.cardBgColor,
             ),
-          ),
-          contentPadding: EdgeInsets.only(
-            top: 0.0,
-          ),
-          content: Container(
-            height: MediaQuery.of(context).size.height * 0.6,
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Container(
-                    height: 400,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: CachedNetworkImage(
-                          height: 400,
-                          imageUrl: merch.url.toString(),
-                          imageBuilder: (context, imageProvider) => Container(
-                            height: 400,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: imageProvider, fit: BoxFit.fitHeight),
-                            ),
-                          ),
-                          placeholder: (context, url) => Shimmer.fromColors(
-                            baseColor: AppColors.primaryColor,
-                            highlightColor: Colors.grey,
+            height: MediaQuery.of(context).size.height * 0.92,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: Container(
+                    height: 5,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: AppColors.secondaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      Stack(
+                        children: [
+                          ShaderMask(
+                            shaderCallback: (rect) {
+                              return const LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.center,
+                                colors: [Colors.black, Colors.transparent],
+                              ).createShader(
+                                  Rect.fromLTRB(0, 0, rect.width, rect.height));
+                            },
+                            blendMode: BlendMode.dstOut,
                             child: Container(
-                              color: Colors.grey,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.4,
+                                width: MediaQuery.of(context).size.width,
+                                child: showSizeChart? Container(
+                                  height: 250,
+                                  width: 200,
+                                  child: Image.asset("assets/size.jpg"),
+                                ) :CachedNetworkImage(
+                                  height: 250,
+                                  width: 200,
+                                  imageUrl: merch.url.toString(),
+                                  placeholder: (context, url) =>
+                                      Shimmer.fromColors(
+                                    baseColor: AppColors.primaryColor,
+                                    highlightColor: Colors.grey,
+                                    child: Container(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  errorWidget: (context, url, error) =>
+                                      Image.asset("assets/placeholder.png"),
+                                  fit: BoxFit.cover,
+                                )),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              merch.name.toString().toUpperCase(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 25,
+                                  color: AppColors.secondaryColor,
+                                  fontFamily: GoogleFonts.sora.toString()),
                             ),
-                          ),
-                          errorWidget: (context, url, error) => Image.asset(
-                            'assets/placeholder.png',
-                            fit: BoxFit.fitWidth,
-                          ),
-                        )
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(8, 8, 10, 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          merch.name,
-                          style: GoogleFonts.sora(
-                              color: AppColors.primaryColor,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 20),
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  showSizeChart = !showSizeChart;
+                                });
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 20),
+                                child: Text(
+                                  "Size Chart",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 15,
+                                      color: showSizeChart?AppColors.highlightColor:AppColors.secondaryColor,
+                                      fontFamily: GoogleFonts.sora.toString()),
+                                ),
+                              ),
+                            )
+                          ],
                         ),
-                        Text(
-                          "Rs ${merch.price}",
-                          style: GoogleFonts.sora(
-                              color: AppColors.highlightColor,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8, 2, 8, 8),
-                    child: Text(
-                      merch.desc,
-                      style: TextStyle(fontSize: 12),
-                    ),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 60,
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        _launchURLBrowser("https://vtop.vit.ac.in/vtop/login", context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: AppColors.primaryColor,
-                        // fixedSize: Size(250, 50),
                       ),
-                      child: Text(
-                        "BUY NOW",
+                      SizedBox(
+                        height: 10,
                       ),
-                    ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Text(
+                          "Description",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                              color: AppColors.secondaryColor,
+                              fontFamily: GoogleFonts.sora.toString()),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Text(
+                          merch.desc.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 15,
+                              color: Colors.grey,
+                              fontFamily: GoogleFonts.sora.toString()),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        10, 0, 10, 0),
+                                    child: Text(
+                                      "\u{20B9}${merch.price}",
+                                      style: TextStyle(
+                                        color: AppColors.highlightColor,
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                                child: Container(
+                              height: MediaQuery.of(context).size.height * 0.06,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 4, 10, 4),
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: AppColors.highlightColor,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    _launchURLBrowser(
+                                        "https://vtop.vit.ac.in/vtop", context);
+                                  },
+                                  child: Text(
+                                    'BUY NOW',
+                                    style: TextStyle(
+                                      color: AppColors.secondaryColor,
+                                      fontSize: 15,
+                                      fontFamily: GoogleFonts.sora.toString(),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                )
+              ],
             ),
-          ),
-        );
+          );
+        });
       });
 }
+
 
 void _launchURLBrowser(_url, BuildContext context) async {
   final Uri _uri = Uri.parse(_url);
@@ -244,4 +362,3 @@ void _launchURLBrowser(_url, BuildContext context) async {
         "The link may be null or may have some issues.", context);
   }
 }
-
